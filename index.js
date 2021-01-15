8ab708c351de602462f1e12fd258ce4796b4c0e2
Jacob Harvey <jacobharv00@gmail.com>
2021-01-15 18:45:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// write your code below!
+//1. write a function named happyHolidays
+//2. make function accept NO parameters.
+//3. make return string Happy holidays!
+function happyHolidays() {
+  return'Happy holidays!'
+}
